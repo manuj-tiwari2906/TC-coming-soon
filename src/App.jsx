c7086af310eb103c541e2d9141cbf3ea313d1b31
manujt <manuka72tiwari@gmail.com>
2024-03-02 11:26:19 +0530

    dependency-check

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
+import React from 'react';
 import "./App.scss";
 import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
-import Home from "./home";
-import WhoAreWe from "./who-are-we";
+const Home = React.lazy(() => import('./home'));
+const WhoAreWe = React.lazy(() => import('./who-are-we'));
 import Navbar from "./navbar";
 import { useLocation } from 'react-router-dom';
 
@@ -10,7 +11,7 @@ function App() {
   const location = useLocation();
   return (
     <>
-        <div style={{ backgroundImage: location.pathname.includes('who-we-are') ? 'unset' : 'url("../src/assets/home-bg.jpeg")' }} className="App">
+        <div style={{ backgroundImage: location.pathname.includes('who-we-are') ? 'unset' : 'url("https://imgur.com/nzWdK72.png")' }} className="App">
           <div className="navbars">
             <Navbar />
           </div>
